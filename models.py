0ad8a1e231491715f392cdaa910e9a641a61c5c3
daggupatimahesh291 <57830482+daggupatimahesh291@users.noreply.github.com>
2019-11-18 11:57:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import datetime
 class notes(models.Model):  
     tittle = models.CharField(max_length=20)  
     desc = models.CharField(max_length=100)  
-    createdate = models.DateField(_("Date"), default=datetime.date.today)
+    createdate = models.CharField(max_length=100)
     updatedate = models.DateField(_("Date"), default=datetime.date.today)  
     class Meta:  
         db_table = "note"  

795871fc8c2792d5d537280ec7f11f380bbc9f04
daggupatimahesh291 <57830482+daggupatimahesh291@users.noreply.github.com>
2019-11-18 11:37:56 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 
 
 # Create your models here.
-from django.db import models  
-class Employee(models.Model):  
-    eid = models.CharField(max_length=20)  
-    ename = models.CharField(max_length=100)  
-    eemail = models.EmailField()  
-    econtact = models.CharField(max_length=15)  
+from django.db import models 
+import datetime
+class notes(models.Model):  
+    tittle = models.CharField(max_length=20)  
+    desc = models.CharField(max_length=100)  
+    createdate = models.DateField(_("Date"), default=datetime.date.today)
+    updatedate = models.DateField(_("Date"), default=datetime.date.today)  
     class Meta:  
-        db_table = "employee"  
+        db_table = "note"  
